1e60dddb6905a0bf006406ac13d6bcf442f684fe
Alex Gurnet <alexandergurnet@gmail.com>
2020-07-30 15:48:52 +0300

    promisified fs.readFile and added .eslintignore file

--- routes/cards.js
+++ routes/cards.js
@@ -1,17 +1,20 @@
 const cardsRouter = require('express').Router();
 const path = require('path');
+const util = require('util');
 const fs = require('fs');
 
-const filapath = path.join(path.dirname(process.mainModule.filename), '/data/cards.json');
+const filepath = path.join(path.dirname(process.mainModule.filename), './data/cards.json');
 
 cardsRouter.get('/cards', (req, res) => {
-  fs.readFile(filapath, { encoding: 'utf8' }, (err, data) => {
-    if (err) {
-      return null;
-    }
-
-    res.send(data);
-  });
+  const readFile = util.promisify(fs.readFile);
+  readFile(filepath, { encoding: 'utf8' })
+    .then((data) => {
+      const dataParsed = JSON.parse(data);
+      res.send(dataParsed);
+    })
+    .catch(() => {
+      res.status(500);
+    });
 });
 
 module.exports = cardsRouter;
--- routes/users.js
+++ routes/users.js
@@ -1,33 +1,38 @@
 const usersRouter = require('express').Router();
 const path = require('path');
+const util = require('util');
 const fs = require('fs');
 
-const filapath = path.join(path.dirname(process.mainModule.filename), '/data/users.json');
+const filepath = path.join(path.dirname(process.mainModule.filename), '/data/users.json');
+const readFile = util.promisify(fs.readFile);
 
 usersRouter.get('/users', (req, res) => {
-  fs.readFile(filapath, { encoding: 'utf8' }, (err, data) => {
-    if (err) {
-      return null;
-    }
-
-    res.send(data);
-  });
+  readFile(filepath, { encoding: 'utf8' })
+    .then((data) => {
+      const dataParsed = JSON.parse(data);
+      res.send(dataParsed);
+    })
+    .catch(() => {
+      res.status(500);
+    });
 });
 
 usersRouter.get('/users/:id', (req, res) => {
-  fs.readFile(filapath, { encoding: 'utf8' }, (err, data) => {
-    if (err) {
-      return null;
-    }
-    const dataParsed = JSON.parse(data);
-    const userFound = dataParsed.find((user) => req.params.id === user._id);
-    if (userFound) {
-      res.send(userFound);
-    } else {
-      res.status(404);
-      res.send({ message: 'Нет пользователя с таким id' });
-    }
-  });
+  readFile(filepath, { encoding: 'utf8' })
+    .then((data) => {
+      const dataParsed = JSON.parse(data);
+      // eslint-disable-next-line no-underscore-dangle
+      const userFound = dataParsed.find((user) => req.params.id === user._id);
+      if (userFound) {
+        res.send(userFound);
+      } else {
+        res.status(404);
+        res.send({ message: 'Нет пользователя с таким id' });
+      }
+    })
+    .catch(() => {
+      res.status(500);
+    });
 });
 
 usersRouter.get('*', (req, res) => {
